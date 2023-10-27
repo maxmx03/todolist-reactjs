2bd0277f46a85349a6d8278080e1cffab80ace0d
Max Del Canto <milianordelcanto@gmail.com>
2023-10-27 10:46:24 -0300

    feat: persist state to localstorage

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,10 @@ import projectReducer from './projectSlicer'
 import todoReducer from './todoSlicer'
 import todoChartReducer from './todoChartSlice'
 import projectChartReducer from './projectChartSlicer'
+import { loadState, saveState } from '../utils/localStorage'
+import _ from 'lodash'
+
+const persistedState = loadState()
 
 export const store = configureStore({
   reducer: {
@@ -11,4 +15,14 @@ export const store = configureStore({
     todoChart: todoChartReducer,
     projectChart: projectChartReducer,
   },
+  preloadedState: persistedState,
 })
+
+store.subscribe(_.throttle(() => {
+  saveState({
+    todos: store.getState().todos,
+    projects: store.getState().projects,
+    todoChart: store.getState().todoChart,
+    projectChart: store.getState().projectChart,
+  })
+}, 1000))
